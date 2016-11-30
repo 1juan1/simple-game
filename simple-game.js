1adc697df085da620074e63c62f4a0a77975d2b9
Juan Yepez <yepezjuan6@gmail.com>
2016-11-29 20:43:20 -0800

    addded overlap to my food and player

--- simple-game.js
+++ simple-game.js
@@ -5,6 +5,7 @@ var canvas =  document.getElementById("canvas")
 
 //////////////the birds movement////////////////////////////
 var xpos = 250
+var ypos = 200
 document.addEventListener("keydown", function(e) {
 if (e.keyCode == 37) {
    document.getElementById("bird").setAttribute("x", xpos - 15)
@@ -15,12 +16,7 @@ if (e.keyCode == 37) {
   document.getElementById("bird").setAttribute("x", xpos + 15)
   xpos = xpos + 15;
 }
-})
-
-
-var ypos = 200
-document.addEventListener("keydown", function(e) {
-if (e.keyCode == 38) {
+else if (e.keyCode == 38) {
    document.getElementById("bird").setAttribute("y", ypos - 15)
   ypos = ypos - 15;
 }
@@ -29,23 +25,37 @@ if (e.keyCode == 38) {
   document.getElementById("bird").setAttribute("y", ypos + 15)
   ypos = ypos + 15;
 }
+
+// check for overlap
+
+var bird1 = document.getElementById("bird");
+var taco = document.getElementById("taco");
+var taco1 = document.getElementById('taco2');
+
+var birdx = Number(bird1.getAttribute("x"));
+var birdy = Number(bird1.getAttribute("y"));
+
+//first food
+var foodx = Number(taco.getAttribute("x"));
+var foody = Number(taco.getAttribute("y"));
+var foodwidth = Number(taco.getAttribute("width"));
+var foodheight = Number(taco.getAttribute("height"));
+
+//second food
+var foodx1 = Number(taco1.getAttribute("x"));
+var foody1 = Number(taco1.getAttribute("y"));
+var foodwidth1 = Number(taco1.getAttribute("width"));
+var foodheight1 = Number(taco1.getAttribute("height"));
+
+
+if (birdx > foodx && birdx < foodx + foodwidth && birdy > foody &&  birdy < foody + foodheight) {
+  console.log("overlap");
+}
+
+
+ if (birdx > foodx1 && birdx < foodx1 + foodwidth1 && birdy > foody1 &&  birdy < foody1 + foodheight1) {
+  console.log('overlap');
+}
 })
 
 /////overlap///////
-canvas.addEventListener("click" ,function (e) {
-  var bird1 = document.getElementById.attribute("bird")
-  var taco1 = document.getElementById.attribute("taco")
-
-birdx = Number(bird1.getAttribute("x"))
-birdy = Number(bird1.getAttribute("y"))
-
-foodx = Number(taco1.getAttribute("x"))
-foody = Number(taco1.getAttribute("y"))
-
-  if (birdx  > foodx && birdx < foodx + width)
-  {
-
-    taco1.setAttribute("begin", "food1.end")
-  
-  }
-})
